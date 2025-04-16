29c0d4231e544b5ab068768cb3ca2ef0021132d5
VladislavLinyaev <147928300+VladislavLinyaev@users.noreply.github.com>
2025-04-16 14:48:35 +0300

    Add files via upload
    написал database.h

---
+++ database.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include "Patient.h"
+#include <iostream>
+#include <winsock2.h>
+#include <string>
+
+inline std::string to_lower(const std::string& str);
+
+namespace patient_db {
+	std::string create_patients(SOCKET* client_socket, std::vector<Patient>* arr);
+	void write_patients(std::vector<Patient> arr);
+	void read_patients(std::vector<Patient>* arr);
+	std::string add_patient(SOCKET* client_socket, std::vector<Patient>* arr);
+	std::string search_patient(SOCKET* client_socket, std::vector<Patient> arr);
+	std::string delete_patient(SOCKET* client_socket, std::vector<Patient>* arr);
+	void advance_time(std::vector<Patient>* arr, int days);
+	std::string print_patients(SOCKET* client_socket, std::vector<Patient> arr);
+}
